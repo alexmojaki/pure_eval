566a2b4b28cd9ddf6a02b370a2a5d409ea651a58
KOLANICH <kolan_n@mail.ru>
2020-02-12 18:12:36 +0300

    Moved the metadata into setup.cfg

--- pure_eval/__init__.py
+++ pure_eval/__init__.py
@@ -1,4 +1,3 @@
 from .core import Evaluator, CannotEval, group_expressions, is_expression_interesting
 from .my_getattr_static import getattr_static
-
-__version__ = '0.0.3'
+from .version import __version__
