7dcc05be1e3baf873e3eb4020d9739d68ec991ba
Alex Hall <alex.mojaki@gmail.com>
2020-05-30 20:00:01 +0200

    Update packaging to match others

--- make_release.sh
+++ make_release.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env bash
-set -eux;
+set -eux
 
-if [${1+x}]; then
-    if [[ ${1} =~ ^v?([0-9]+)(\.[0-9]+)?(\.[0-9]+)?$ ]]; then
-        :;
-    else
-        echo "Not a valid release tag.";
-        exit 1;
-    fi;
+# Ensure that there are no uncommitted changes
+# which would mess up using the git tag as a version
+[ -z "$(git status --porcelain)" ]
+
+if [ -z "${1+x}" ]
+then
+    set +x
+    echo Provide a version argument
+    echo "${0} <major>.<minor>.<patch>"
+    exit 1
 else
-    echo "${0} <major>.<minor>.<patch>";
-    exit 1;
-fi;
+    if [[ ${1} =~ ^([0-9]+)(\.[0-9]+)?(\.[0-9]+)?$ ]]; then
+        :
+    else
+        echo "Not a valid release tag."
+        exit 1
+    fi
+fi
 
-export TAG="v${1}";
-echo "$TAG"
-git tag "${TAG}";
-git push origin master "${TAG}";
-rm -rf ./build ./dist;
-python3 -m pep517.build -b .;
-twine upload ./dist/*.whl;
+tox -p auto
+
+export TAG="v${1}"
+git tag "${TAG}"
+git push origin master "${TAG}"
+rm -rf ./build ./dist
+python3 -m pep517.build -b .
+twine upload ./dist/*.whl
